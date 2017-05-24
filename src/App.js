c51130788bcaf8d3574062e00a41eb990e971682
bhavya <bhavya@happay.in>
2017-05-24 16:57:09 +0530

    implemented basic calendar using react

---
+++ src/App.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import BigCalendar from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import moment from 'moment';
+import logo from './logo.svg';
+import './App.css';
+
+BigCalendar.setLocalizer(
+  BigCalendar.momentLocalizer(moment)
+);
+
+let allViews = Object.keys(BigCalendar.views).map(k => BigCalendar.views[k]);
+let events = [
+  {
+    'title': 'All Day Event',
+    'allDay': true,
+    'start': new Date(2015, 3, 0),
+    'end': new Date(2015, 3, 1)
+  },
+  {
+    'title': 'Long Event',
+    'start': new Date(2015, 3, 7),
+    'end': new Date(2015, 3, 10)
+  },
+
+  {
+    'title': 'DTS STARTS',
+    'start': new Date(2016, 2, 13, 0, 0, 0),
+    'end': new Date(2016, 2, 20, 0, 0, 0)
+  },
+
+  {
+    'title': 'DTS ENDS',
+    'start': new Date(2016, 10, 6, 0, 0, 0),
+    'end': new Date(2016, 10, 13, 0, 0, 0)
+  },
+
+  {
+    'title': 'Some Event',
+    'start': new Date(2015, 3, 9, 0, 0, 0),
+    'end': new Date(2015, 3, 9, 0, 0, 0)
+  },
+  {
+    'title': 'Conference',
+    'start': new Date(2015, 3, 11),
+    'end': new Date(2015, 3, 13),
+    desc: 'Big conference for important people'
+  },
+  {
+    'title': 'Meeting',
+    'start': new Date(2015, 3, 12, 10, 30, 0, 0),
+    'end': new Date(2015, 3, 12, 12, 30, 0, 0),
+    desc: 'Pre-meeting meeting, to prepare for the meeting'
+  },
+  {
+    'title': 'Lunch',
+    'start':new Date(2015, 3, 12, 12, 0, 0, 0),
+    'end': new Date(2015, 3, 12, 13, 0, 0, 0),
+    desc: 'Power lunch'
+  },
+  {
+    'title': 'Meeting',
+    'start':new Date(2015, 3, 12,14, 0, 0, 0),
+    'end': new Date(2015, 3, 12,15, 0, 0, 0)
+  },
+  {
+    'title': 'Happy Hour',
+    'start':new Date(2015, 3, 12, 17, 0, 0, 0),
+    'end': new Date(2015, 3, 12, 17, 30, 0, 0),
+    desc: 'Most important meal of the day'
+  },
+  {
+    'title': 'Dinner',
+    'start':new Date(2015, 3, 12, 20, 0, 0, 0),
+    'end': new Date(2015, 3, 12, 21, 0, 0, 0)
+  },
+  {
+    'title': 'Birthday Party',
+    'start':new Date(2015, 3, 13, 7, 0, 0),
+    'end': new Date(2015, 3, 13, 10, 30, 0)
+  }
+]
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <BigCalendar  {...this.props}  defaultDate={new Date(2015, 3, 1)} views={allViews} events={events}
+/>
+      </div>
+    );
+  }
+}
+
+export default App;
